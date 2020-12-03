126e69345b3ea5760475708a7bf01bf7f9acce05
David F <Skilliciousboy@icloud.com>
2020-12-03 19:53:27 +0000

    This is a new commit for what I originally planned to be amended

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
+import Main from './components/Main'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Quick Kanji</h1>
-    </div>
+    <Main />
   );
 }
 
